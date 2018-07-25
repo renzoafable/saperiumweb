adc9251a8d3fb11fbfbc71e876b88f255c4311dc
renzoafable <lbafable@up.edu.ph>
2018-07-25 16:13:21 +0800

    Added contact us feature

--- server/src/entities/contactUs/repo.js
+++ server/src/entities/contactUs/repo.js
@@ -2,22 +2,70 @@
 var SqlString = require('sqlstring');
 
 const repoContactUs = function (db) {
-    const repo = {
-        editCareers: (title, address,  body) => {
-            return new Promise((resolve, reject) => {
-                const values = [title, address, body];
-                const queryString = SqlString.format(
-                    `CALL edit_contact_us(?, ?, ?)`, values
-                );
+  const repo = {
+    editContactUs: (title, address, body) => {
+      return new Promise((resolve, reject) => {
+        const values = [title, address, body];
+        const queryString = SqlString.format(
+          `CALL edit_contact_us(?, ?, ?)`, values
+        );
 
-                db.query(queryString, (err, results) => {
-                    if (err) return reject(500)
-                    return resolve(results)
-                });
-            });
-        }
+        db.query(queryString, (err, results) => {
+          if (err) return reject(500)
+          return resolve(results)
+        });
+      });
+    },
+    getContactUs: () => {
+      return new Promise((resolve, reject) => {
+        const queryString = SqlString.format(
+          `SELECT * FROM CONTACT_US`
+        );
+
+        db.query(queryString, (err, result) => {
+          if (err) {
+            console.log(err.message);
+            return reject(500);
+          }
+
+          return resolve(result);
+        });
+      });
+    },
+    getPhoneNums: () => {
+      return new Promise((resolve, reject) => {
+        const queryString = SqlString.format(
+          `SELECT * FROM PHONE`
+        );
+
+        db.query(queryString, (err, result) => {
+          if (err) {
+            console.log(err.message);
+            return reject(500);
+          }
+
+          return resolve(result);
+        });
+      });
+    },
+    addPhoneNums: (givenNumber) => {
+      return new Promise((resolve, reject) => {
+        const queryString = SqlString.format(
+          `CALL add_phone(?)`, givenNumber
+        );
+        
+        db.query(queryString, (err, result) => {
+          if (err) {
+            console.log(err.message);
+            return reject(500);
+          }
+
+          return resolve();
+        });
+      });
     }
-    return repo;
+  }
+  return repo;
 }
 
 module.exports = repoContactUs;
--- server/src/entities/contactUs/router.js
+++ server/src/entities/contactUs/router.js
@@ -6,6 +6,9 @@ const contactUsRepo = require('./repo')(db);
 const contactUsCtrl = require('./controller')(contactUsRepo);
 
 // place methods here
-
+router.put('/', contactUsCtrl.editContactUs);
+router.get('/', contactUsCtrl.getContactUs);
+router.get('/phones', contactUsCtrl.getPhoneNums);
+router.post('/phones', contactUsCtrl.addPhoneNums);
 
 module.exports = router;
