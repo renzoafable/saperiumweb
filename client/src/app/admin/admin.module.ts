3c2e008f3c988c5bcf8d9e483e9e3bef72a10b27
Mikee <mikee.jazmines@gmail.com>
2018-07-26 12:23:13 +0800

    Created all components

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ApplyComponent } from './apply/apply.component';
+import { CareersComponent } from './careers/careers.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [LoginComponent, AboutComponent, ApplyComponent, CareersComponent, ContactUsComponent, HomeComponent, ServicesComponent]
 })
 export class AdminModule { }
