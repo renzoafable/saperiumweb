d7f8ea0a42ac76660d0b9c4ce26ff2920cbf15be
Mikee <mikee.jazmines@gmail.com>
2018-07-26 13:41:04 +0800

    Fixed navbar for admin side

--- client/src/app/admin/admincareers/admincareers.component.ts
+++ client/src/app/admin/admincareers/admincareers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CareersService } from '../../shared/services/careers.service';
+
 @Component({
   selector: 'app-admincareers',
   templateUrl: './admincareers.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdmincareersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private careersService: CareersService) { }
+
+  title = 'Careers';
+  body = 'Fake body';
+  getCareers() {
+    this.careersService.getCareers().subscribe((result) => {
+      this.title = result.result[0].title;
+      this.body = result.result[0].body;
+    }, error => {
+      console.log(error.error.error);
+    });
+  }
 
   ngOnInit() {
+    this.getCareers();
   }
 
 }
--- client/src/app/shared/components/navbar/navbar.component.ts
+++ client/src/app/shared/components/navbar/navbar.component.ts
@@ -9,6 +9,8 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  isAdmin = false;
+
   ngOnInit() {
   }
 
